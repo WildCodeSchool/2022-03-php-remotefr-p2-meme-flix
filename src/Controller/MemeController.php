d774b08f7bcbe7a4358b3d22e930ae0f94d65f28
Yoann Brambilla <yobrambilla@gmail.com>
2022-04-21 06:29:46 +0200

    fix form

---
+++ src/Controller/MemeController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+class MemeController extends AbstractController
+{
+    /**
+     * Display home page
+     */
+    public function createMeme(): string
+    {
+        return $this->twig->render('meme/create.html.twig');
+
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+            $errors = [];
+
+            $uploadDir = 'assets/images/';
+
+            $extension = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
+
+            $fileName = uniqid() . '.' . $extension;
+
+            $uploadFile = $uploadDir . basename($fileName);
+
+            move_uploaded_file($_FILES['image']['tmp_name'], $uploadFile);
+
+            $authorizedExtensions = ['jpg', 'gif', 'webp', 'png'];
+
+            $maxFileSize = 1000000;
+
+            if ((!in_array($extension, $authorizedExtensions))) {
+                $errors[] = 'Format invalide (gif, jpg, png, webp)';
+            }
+
+            if (file_exists($_FILES['image']['tmp_name']) && filesize($_FILES['image']['tmp_name']) > $maxFileSize) {
+                $errors[] = 'Ton image doit faire moins de 1Mo';
+            }
+        }
+    }
+}
