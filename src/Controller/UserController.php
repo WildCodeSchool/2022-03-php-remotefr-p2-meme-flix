252a3941a07b788efca6358ac73ba13a49e64b90
Yoann Brambilla <yobrambilla@gmail.com>
2022-05-02 08:11:22 +0200

    us_8 replace no-commit files

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,6 +10,7 @@ class UserController extends AbstractController
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $credentials = array_map('trim', $_POST);
+            $dataErrors = [];
 
             if (empty($credentials["email"])) {
                 $dataErrors[] = "Email is required";
@@ -21,13 +22,10 @@ class UserController extends AbstractController
 
             if (isset($_POST["submit"]) && (!$dataErrors)) {
                 $userManager = new UserManager();
-                // On demande au UserManager de rechercher l'utilisateur en BDD à partir de l'email
                 $user = $userManager->selectOneByEmail($credentials['email']);
-                // Si l'utilisateur a été trouvé et si l'empreinte de son mot de passe est vérifiée...
+
                 if ($user && password_verify($credentials['password'], $user['password'])) {
-                    // ...alors on persiste l'id de notre utilisateur identifié dans la session PHP à l'index ['user_id']
                     $_SESSION['user_id'] = $user['id'];
-                    // puis on le redirige sur une autre page (page d'accueil ici)
                     header('Location: /');
                     return null;
                 }
@@ -61,19 +59,17 @@ class UserController extends AbstractController
             if (!filter_var($credentials["email"], FILTER_VALIDATE_EMAIL)) {
                 $dataErrors[] = "invalid email format";
             }
-            $hash = password_hash($credentials['password'], PASSWORD_DEFAULT);
-            $pattern = '/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{8,12}$/';
+            // $hash = password_hash($credentials['password'], PASSWORD_DEFAULT);
+            // $pattern = '/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{8,12}$/';
 
-            if ($hash === false && strlen($hash) <= 8 && preg_match($pattern, $hash)) {
-                $dataErrors[] = "invalid password format";
-            }
-
+            // if ($hash === false && strlen($hash) <= 8 && preg_match($pattern, $hash)) {
+            //     $dataErrors[] = "invalid password format";
+            // }
 
             if (isset($_POST["submit"]) && (!$dataErrors)) {
                 $userManager = new UserManager();
                 $userId = $userManager->insert($credentials);
                 if ($userId) {
-
                     $_SESSION['user_id'] = $userId;
                     header("Location: /welcome");
                 }
@@ -89,4 +85,3 @@ class UserController extends AbstractController
         return $this->twig->render('Users/welcome.html.twig');
     }
 }
-
